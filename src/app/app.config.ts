d82c8c5b006a4bd0ed13c7585014721726a50866
Muchachos <indijanero@gmail.com>
2018-02-08 15:39:36 +0100

    Adding client url endpoin to app.config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,6 @@
 export class AppConfig {
     public static readonly apiUrl: string = 'http://localhost:55405/api';
+    public static readonly clientUrl: string = 'http://localhost:4200';
 }
 
 
--- src/app/shared/interceptors/auth.interceptor.ts
+++ src/app/shared/interceptors/auth.interceptor.ts
@@ -1,7 +1,8 @@
 import { HttpInterceptor, HttpHandler, HttpHeaderResponse, HttpSentEvent, HttpProgressEvent, HttpResponse, HttpUserEvent, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
-import { AuthService } from '../../auth/auth.service';
 import { Injectable } from '@angular/core';
+import { AuthService } from '../../auth/auth.service';
+import { AppConfig } from '../../app.config';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
@@ -9,7 +10,7 @@ export class AuthInterceptor implements HttpInterceptor {
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpSentEvent | HttpHeaderResponse | HttpProgressEvent | HttpResponse<any> | HttpUserEvent<any>> {
         req.headers.append('Authorization', `Bearer ${this.authService.getToken()}`);
-        req.headers.append('Access-Control-Allow-Origin', 'http://localhost:4200');
+        req.headers.append('Access-Control-Allow-Origin', AppConfig.clientUrl);
         req.headers.append('Access-Control-Allow-Credentials', 'true');
         return next.handle(req);
     }
