cc409556e5e8c0b1c9d38dafc5ac8de6f6432120
Muchachos <indijanero@gmail.com>
2018-02-08 05:31:26 +0100

    Cleaning app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { ToastModule } from 'ng2-toastr/ng2-toastr';
+
 import { MealsModule } from './meals/meals.module';
 import { AuthModule } from './auth/auth.module';
 import { OrdersModule } from './orders/orders.module';
